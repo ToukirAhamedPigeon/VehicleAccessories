fd1f9a325a7d5afb184f099679cbaeff15605b57
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@hotmail.com>
2017-12-02 12:39:39 +0600

    posting bug fixing

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -38,39 +38,46 @@ class Home extends MY_Controller {
 	
 	public function login()
 	{
-		if ($this->form_validation->run('login') == FALSE)
+		if($_POST)
 		{
-			$this->load->view('login');
-		}
-		else
-		{
-			$post=$this->input->post();
-			unset($post['submit']);
-			$this->load->model('userModel');
-			if($user=$this->userModel->getUser($post['username']))
+			if ($this->form_validation->run('login') == FALSE)
 			{
-				$id=$user->id;
-				$password=$user->password;
-				$type=$user->type;
-				if($post['password']===$password)
-				{
-					$this->session->set_userdata('userid',$id);
-					$this->session->set_userdata('usertype',$type);
-
-
-					redirect('/User/index/'.$id, 'refresh');
-				}
-				else
-				{
-					$check_error='Password is not correct.'; 
-					$this->load->view('login',compact('check_error'));
-				}
+				$this->load->view('login');
 			}
 			else
 			{
-				$check_error='Invalid Username'; 
-				$this->load->view('login',compact('check_error'));
+				$post=$this->input->post();
+				unset($post['submit']);
+				$this->load->model('userModel');
+				if($user=$this->userModel->getUser($post['username']))
+				{
+					$id=$user->id;
+					$password=$user->password;
+					$type=$user->type;
+					if($post['password']===$password)
+					{
+						$this->session->set_userdata('userid',$id);
+						$this->session->set_userdata('usertype',$type);
+
+
+						redirect('/User/index/'.$id, 'refresh');
+					}
+					else
+					{
+						$check_error='Password is not correct.'; 
+						$this->load->view('login',compact('check_error'));
+					}
+				}
+				else
+				{
+					$check_error='Invalid Username'; 
+					$this->load->view('login',compact('check_error'));
+				}
 			}
+		}
+		else
+		{
+			redirect('/Home/showlogin', 'refresh');
 		}
 	}
 
@@ -81,56 +88,62 @@ class Home extends MY_Controller {
 	
 	public function registration()
 	{
-		if ($this->form_validation->run('registration') == FALSE)
-		{
-			$this->load->view('registrationuser');
-		}
-		else
-		{
-			$post=$this->input->post();
-			unset($post['submit']);
-			unset($post['confirmpassword']);
-			$filename=rand(100,999).$post['username'].getDateTime(2);
-			$config=['upload_path'=> './assets/files/profile',
-			'allowed_types'=>'jpg|gif|png|jpeg',
-			'file_name'=>$filename,
-			'max_size'=>'10240'
-		];
-		$this->load->library('upload',$config);
-		if($this->upload->do_upload('profile')){
-			$fileAbout=$this->upload->data('profile');
-			$this->load->model('userModel');
-			$this->load->model('filesModel');
-			if($this->userModel->addUser($post))
+		if($_POST){
+			if ($this->form_validation->run('registration') == FALSE)
 			{
-				$user=$this->userModel->getUser($post['username']);
-
-				$id=$user->id;
-				$fileinfo=array('fileholder'=>$id,'filestatus'=>'profile','filepath'=>'assets/files/profile/'.$fileAbout['orig_name'],'holdertype'=>'user');
-				if($this->filesModel->uploadFile($fileinfo))
-				{
-					$this->session->set_flashdata('feedback','Registration Successful. Login with Username and Password.');
-					redirect('/Home/showlogin', 'refresh');
-				}
-				else
-				{
-					$data['upload_error']='Adding Profile Picture process has failed';
-					$this->load->view('registrationuser',compact($data));
-				}
+				$this->load->view('registrationuser');
 			}
 			else
 			{
-				$upload_error='Adding User process has failed';
+				$post=$this->input->post();
+				unset($post['submit']);
+				unset($post['confirmpassword']);
+				$filename=rand(100,999).$post['username'].getDateTime(2);
+				$config=['upload_path'=> './assets/files/profile',
+				'allowed_types'=>'jpg|gif|png|jpeg',
+				'file_name'=>$filename,
+				'max_size'=>'10240'
+			];
+			$this->load->library('upload',$config);
+			if($this->upload->do_upload('profile')){
+				$fileAbout=$this->upload->data('profile');
+				$this->load->model('userModel');
+				$this->load->model('filesModel');
+				if($this->userModel->addUser($post))
+				{
+					$user=$this->userModel->getUser($post['username']);
 
+					$id=$user->id;
+					$fileinfo=array('fileholder'=>$id,'filestatus'=>'profile','filepath'=>'assets/files/profile/'.$fileAbout['orig_name'],'holdertype'=>'user');
+					if($this->filesModel->uploadFile($fileinfo))
+					{
+						$this->session->set_flashdata('feedback','Registration Successful. Login with Username and Password.');
+						redirect('/Home/showlogin', 'refresh');
+					}
+					else
+					{
+						$data['upload_error']='Adding Profile Picture process has failed';
+						$this->load->view('registrationuser',compact($data));
+					}
+				}
+				else
+				{
+					$upload_error='Adding User process has failed';
+
+					$this->load->view('registrationuser',compact('upload_error'));
+				}
+
+			}
+			else
+			{
+				$upload_error=$this->upload->display_errors();
 				$this->load->view('registrationuser',compact('upload_error'));
 			}
-
 		}
-		else
-		{
-			$upload_error=$this->upload->display_errors();
-			$this->load->view('registrationuser',compact('upload_error'));
-		}
+	}
+	else
+	{
+		redirect('/Home/showRegistration', 'refresh');
 	}
 }
 
